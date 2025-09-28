d863ca4abfb79045e860a23cf66d63cf95423df5
Francisco Douglas <franciscodouglas77@outlook.com>
2025-09-28 07:53:55 -0300

    -----

---
+++ PHP_Basico/01_Introducao/04_Variaveis.php
@@ -0,0 +1,12 @@
+<?php
+
+//Definindo variaveis em PHP
+$valor = 100; // Variável do tipo inteiro
+$nome = "João"; // Variável do tipo string
+$_idade = 25; // Variável do tipo inteiro
+$altura = 1.75; // Variável do tipo float
+
+//Exibindo o valor das variaveis
+echo "Nome: " .$nome . "\n"; // Concatenação de strings
+echo "Idade: " .$_idade . " anos\n";
+echo "Altura: " .$altura . " metros\n";
