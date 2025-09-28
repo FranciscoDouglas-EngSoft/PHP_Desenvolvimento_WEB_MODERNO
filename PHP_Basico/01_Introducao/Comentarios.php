7023f7e904c6a1b1491f97b48d866d2f6bc2c1c6
Francisco Douglas <franciscodouglas77@outlook.com>
2025-09-28 07:46:44 -0300

    -----

---
+++ PHP_Basico/01_Introducao/Comentarios.php
@@ -0,0 +1,10 @@
+#Comentarios em PHP
+
+<?php
+// Este é um comentário de uma linha em PHP
+# Este também é um comentário de uma linha em PHP
+/* Este é um comentário de múltiplas linhas em PHP
+   Você pode escrever várias linhas aqui
+*/
+echo "Olá, Mundo!"; // Exibe uma mensagem na tela
+?>
