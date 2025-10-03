03668d239ca75ef9b8183af6b480a96e29d28736
Francisco Douglas <franciscodouglas77@outlook.com>
2025-10-03 04:43:07 -0300

    ------

---
+++ PHP_Basico/08_Require_Include/8.6_meio.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <nav>
+        <a href="./8.5_inicio.php">Início</a>
+        <a href="./8.6_meio.php">Meio</a>
+        <a href="./8.7_fim.php">Fim</a>
+    </nav>
+
+    <h1>MEIO</h1>
+
+    <hr>
+    <p>Pagina Inicial</p>
+
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae quia facilis labore consectetur, illum molestias a repellat doloremque culpa neque?</p>
+</body>
+</html>
---
+++ PHP_Basico/08_Require_Include/ExemploPratico/publico/index.php
@@ -0,0 +1,6 @@
+<?php require_once('../estrutura/header.php'); ?>
+    <main>
+        <h2>Conteúdo Principal</h2>
+        <p>Este é o conteúdo principal da página.</p>
+    </main>
+<?php require_once('../estrutura/footer.php'); ?>
